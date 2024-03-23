e8476f831552e6854c55749d98202476a3f0ee9b
lalifeier <lalifeier@gmail.com>
2024-03-24 01:37:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -34,9 +34,11 @@ if (process.env.NODE_ENV === 'production' || !ENABLE_LOG) {
   console.log = function () { };
 }
 
-const uuidv4 = () => ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,c =>
-  (c^Math.random()*16>>c/4).toString(16)
-);
+function uuidv4 () {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ Math.random() * 16 >> c / 4).toString(16)
+  );
+}
 
 // 获取系统信息
 const OS = process.platform;
